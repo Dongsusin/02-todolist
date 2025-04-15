74184e99a155d9851d37528cf12a7841cb3e7ce0
Dongsusin <auroratime020715@gmail.com>
2025-04-15 11:19:35 +0900

    first commit

---
+++ src/components/PrimaryButton.js
@@ -0,0 +1,12 @@
+const PrimaryButton = ({ label, onClick }) => {
+  return (
+    <button
+      className="primary-button"
+      onClick={onClick} // 클릭하면 props로 받은 onClick 함수 실행
+    >
+      {label}
+    </button>
+  );
+};
+
+export default PrimaryButton;
